f1d948048d905b4e5b636cb2fceaad3df70448bc
anders <atl@pck.dk>
2023-03-25 22:40:05 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,8 @@ document.addEventListener('DOMContentLoaded', function() {
     h1.innerText = '';
     var index = 0;
     var speed = 50; // milliseconds per character
-  
+    var blinkSpeed = 1000; // milliseconds per blink
+
     // Start typing animation
     function type() {
         if (text[index] === ' ') {
@@ -13,17 +14,24 @@ document.addEventListener('DOMContentLoaded', function() {
         } else {
             h1.innerHTML += text[index];
         }
-      index++;
-      if (index < text.length) {
-        setTimeout(type, speed);
-      }
+        index++;
+        if (index < text.length) {
+            setTimeout(type, speed);
+        } else {
+            var lastChar = h1.lastChild;
+            var blinkChar = document.createElement('span');
+            blinkChar.classList.add('blink');
+            blinkChar.innerText = lastChar.textContent;
+            lastChar.replaceWith(blinkChar);
+            blinkChar.style.setProperty('--blink-speed', blinkSpeed + 'ms');
+        }
     }
-  
+
     type();
-  
+
     // Detect whether device is in dark or light mode
     var prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)');
-  
+
     // Update styles based on device mode
     function updateColorScheme() {
         if (prefersDarkMode.matches) {
@@ -32,7 +40,7 @@ document.addEventListener('DOMContentLoaded', function() {
             document.body.classList.remove('dark-mode');
         }
     }
-  
+
     // Listen for changes in device mode and update styles accordingly
     prefersDarkMode.addEventListener('change', function() {
         updateColorScheme();
